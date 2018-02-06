20c160b1299a522ffb16426335d0c6d5290208b0
roman <s91630277@gmail.com>
2018-02-07 02:54:38 +0400

    feb 06

--- src/class-name/class-name.js
+++ src/class-name/class-name.js
@@ -1,11 +1,18 @@
 "use strict";
 module.exports = function (context) { return ({
     ClassDeclaration: function (node) {
-        if (!node.id || node.id.type !== 'Identifier') {
-            return;
-        }
-        if (node.id.name && node.id.name.endsWith('Class')) {
-            context.report({ node: node, message: 'Do not use `Class` suffix in class names' });
-        }
+        console.log('ClassDeclaration');
+        // if (!node.id || node.id.type !== 'Identifier') {
+        //     return;
+        // }
+        // if (node.id.name && node.id.name.endsWith('Class')) {
+        //     context.report({ node, message: 'Do not use `Class` suffix in class names' });
+        // }
+    },
+    MethodDefinition: function (node) {
+        console.log('MethodDefinition');
+    },
+    'MethodDefinition:exit': function (node) {
+        console.log('MethodDefinition:exit');
     }
 }); };
--- src/class-name/class-name.ts
+++ src/class-name/class-name.ts
@@ -3,19 +3,20 @@ import { ClassDeclaration, MethodDefinition } from 'estree';
 export = (context) => ({
 
     ClassDeclaration(node: ClassDeclaration) {
-        if (!node.id || node.id.type !== 'Identifier') {
-            return;
-        }
-        if (node.id.name && node.id.name.endsWith('Class')) {
-            context.report({ node, message: 'Do not use `Class` suffix in class names' });
-        }
+        console.log('ClassDeclaration');
+        // if (!node.id || node.id.type !== 'Identifier') {
+        //     return;
+        // }
+        // if (node.id.name && node.id.name.endsWith('Class')) {
+        //     context.report({ node, message: 'Do not use `Class` suffix in class names' });
+        // }
     },
 
-    // MethodDefinition(node: MethodDefinition) {
-    //     console.log('MethodDefinition');
-    // },
+    MethodDefinition(node: MethodDefinition) {
+        console.log('MethodDefinition');
+    },
 
-    // 'MethodDefinition:exit'(node: MethodDefinition) {
-    //     console.log('MethodDefinition:exit');
-    // },
+    'MethodDefinition:exit'(node: MethodDefinition) {
+        console.log('MethodDefinition:exit');
+    },
 });
