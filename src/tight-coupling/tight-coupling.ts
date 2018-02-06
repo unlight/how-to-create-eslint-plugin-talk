16ab74919d050cf129e9e779ba6640c709fbdac1
roman <s91630277@gmail.com>
2018-02-07 02:21:29 +0400

    feb 06

--- src/tight-coupling/tight-coupling.ts
+++ src/tight-coupling/tight-coupling.ts
@@ -1,18 +1,10 @@
-import { ClassDeclaration, MethodDefinition } from 'estree';
-
 export = (context) => ({
 
     ClassProperty: (node) => {
 
-        // if (!isInjectable || !node.value) {
-        //     return;
-        // }
-        // const nodeValue = node.value;
-        // if (isNewExpression(nodeValue)
-        //     || isRequireCall(nodeValue)
-        //     || isIdentifierFromImports(nodeValue.name)
-        // ) {
-        //     context.report({ node: nodeValue, message });
-        // }
+        if (node.value && node.value.type === 'NewExpression') {
+            context.report({ node, message: 'Pass dependency through constructor' });
+        }
+
     }
 });
